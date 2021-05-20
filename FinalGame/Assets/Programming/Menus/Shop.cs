eb998d247aaa1efd9577a6bc46ee92c35da33b86
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2021-05-20 13:49:42 -0700

    Added Shop UI

---
+++ FinalGame/Assets/Programming/Menus/Shop.cs
@@ -0,0 +1,35 @@
+﻿/****************
+* Name: Andrew Dahlman-Oeth
+* Date:
+* Desc:
+****************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Reroll()
+    {
+
+    }
+
+    void BuyItem()
+    {
+
+    }
+}
