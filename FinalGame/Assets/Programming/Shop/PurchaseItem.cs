c4fa748d857c6e8d36468ea6335f4c91d956de7c
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2021-05-24 13:19:16 -0700

    Shop updates

---
+++ FinalGame/Assets/Programming/Shop/PurchaseItem.cs
@@ -0,0 +1,77 @@
+﻿/****************
+* Name: Andrew Dahlman-Oeth
+* Date:
+* Desc:
+****************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PurchaseItem : MonoBehaviour
+{
+    public string rarity;
+    public TMP_Text itemPrice;
+    public GameObject title;
+    public GameObject description;
+    public GameObject sprite;
+    public int commonPrice = 10;
+    public int rarePrice = 20;
+    public int legendaryPrice = 50;
+
+
+    private StaticVars StaticVars;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(rarity == "Common")
+        {
+            itemPrice.text = string.Format("{00}G", commonPrice);
+        }
+        if(rarity == "Rare")
+        {
+            itemPrice.text = string.Format("{00}G", rarePrice);
+        }
+        if(rarity == "Legendary")
+        {
+            itemPrice.text = string.Format("{00}G", legendaryPrice);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyItem()
+    {
+        
+        if(rarity == "Common")
+        {
+            if(StaticVars.TotalGold >= commonPrice)
+            {
+                StaticVars.TotalGold -= commonPrice;
+            }
+        }
+
+        if(rarity == "Rare")
+        {
+            if(StaticVars.TotalGold >= rarePrice)
+            {
+                StaticVars.TotalGold -= rarePrice;
+            } 
+        }
+
+        if(rarity == "Legendary")
+        {
+            if(StaticVars.TotalGold >= legendaryPrice)
+            {
+                StaticVars.TotalGold -= legendaryPrice;
+            }   
+        }
+    }
+}
---
+++ FinalGame/Assets/Programming/Shop/ShopReroll.cs
@@ -0,0 +1,31 @@
+﻿/****************
+* Name: Andrew Dahlman-Oeth
+* Date:
+* Desc:
+****************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopReroll : MonoBehaviour
+{
+    public GameObject commonItem;
+    public GameObject rareItem;
+    public GameObject legendaryItem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Reroll()
+    {
+
+    }
+}
