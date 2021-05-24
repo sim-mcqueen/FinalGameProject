a934c3f274f4dbe95611f1d7e82e30191abdd4e4
sim-mcqueen <s.mcqueen@digipen.edu>
2021-05-24 14:16:08 -0700

    Added stuff for shop

---
+++ FinalGame/Assets/Programming/Shop/ItemSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemSetter : MonoBehaviour
+{
+    [Tooltip("0 = Common, 1 = Rare, 2 = Legendary")]
+    public int rarity = 0;
+
+    public TMP_Text TitleText;
+    public TMP_Text DescText;
+    int n = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //n = Random.Range(0, 2);
+        n = 0;
+        if (n == 0)
+        {
+            TitleText.text = string.Format("Motor Oil");
+            DescText.text = string.Format("Increases Turn Speed");
+        }
+    }
+}
