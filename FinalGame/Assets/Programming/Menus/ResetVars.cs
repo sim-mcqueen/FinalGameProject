47745c8375fb13c4f48b00886fa5d66711eedd23
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2021-05-26 13:19:26 -0700

    Add sounds

---
+++ FinalGame/Assets/Programming/Menus/ResetVars.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetVars : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public static int TotalGold = 0;
+        public static float TimePassed = 0f;
+        public static float TurnSpeed = 100f;
+        public static int TotalWaves = 0;
+        public static float TotalAccelerationAmount = 0.1f;
+        public static float TotalDecelerationAmount = 0.01f;
+}
+
+}
