0a5a63077fe83cc995b981f504306aac691fe7d5
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2021-05-26 13:13:38 -0700

    TotalWaves = 0

--- FinalGame/Assets/StaticVars.cs
+++ FinalGame/Assets/StaticVars.cs
@@ -7,7 +7,7 @@ public class StaticVars : MonoBehaviour
     public static int TotalGold = 0;
     public static float TimePassed = 0f;
     public static float TurnSpeed = 100f;
-    public static int TotalWaves = 1;
+    public static int TotalWaves = 0;
     public static float TotalAccelerationAmount = 0.1f;
     public static float TotalDecelerationAmount = 0.01f;
 }
