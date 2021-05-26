1ff1aee12a0c079c050dbe24e0f716933652fea8
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2021-05-26 13:22:03 -0700

    Reset player vars at death

--- FinalGame/Assets/Programming/Menus/ResetVars.cs
+++ FinalGame/Assets/Programming/Menus/ResetVars.cs
@@ -7,12 +7,12 @@ public class ResetVars : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        public static int TotalGold = 0;
-        public static float TimePassed = 0f;
-        public static float TurnSpeed = 100f;
-        public static int TotalWaves = 0;
-        public static float TotalAccelerationAmount = 0.1f;
-        public static float TotalDecelerationAmount = 0.01f;
+        StaticVars.TotalGold = 0;
+        StaticVars.TimePassed = 0f;
+        StaticVars.TurnSpeed = 100f;
+        StaticVars.TotalWaves = 0;
+        StaticVars.TotalAccelerationAmount = 0.1f;
+        StaticVars.TotalDecelerationAmount = 0.01f;
+    }   
 }
 
-}
