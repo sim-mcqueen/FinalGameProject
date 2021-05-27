5e4e429334f1e2a02745ab18b0a2901c070a86a4
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2021-05-26 22:05:11 -0700

    Added main menu animation

---
+++ FinalGame/Assets/Programming/Menus/AsteroidCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCollision : MonoBehaviour
+{
+
+    public GameObject asteroidPS;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Explode();
+    }
+
+
+    void Explode()
+    {
+        GameObject asteroid = Instantiate(asteroidPS, transform.position, Quaternion.identity);
+        asteroid.GetComponent<ParticleSystem>().Play();
+        Destroy(asteroid, 4);
+    }
+}
---
+++ FinalGame/Assets/Programming/Menus/ShootAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAnim : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject shootPoint;
+
+    private Animator shootAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    public void PlayShoot()
+    {
+        shootAnimator.Play("Shoot", -1, 0);
+        GameObject proj = Instantiate(projectile, shootPoint.transform.position, Quaternion.identity) as GameObject;
+        var body = proj.GetComponent<Rigidbody2D>();
+        body.velocity = new Vector3(1, 0, 0) * 10f;
+
+        Destroy(proj, 4f);
+    }
+
+
+}
